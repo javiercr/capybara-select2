093a0b80dc55d17157561c4e6e39af5419fc9d87
Javier Cuevas <javi@diacode.com>
2013-04-16 11:04:20 +0200

    Search select2_container by id instead of text

--- lib/capybara-select2.rb
+++ lib/capybara-select2.rb
@@ -5,19 +5,16 @@ module Capybara
   module Select2
     def select2(value, options = {})
       raise "Must pass a hash containing 'from'" if not options.is_a?(Hash) or not options.has_key?(:from)
-      select_name = options[:from]
-
-
-      select2_container = find(".select2-choice", text: select_name).click
-
+      select_id = options[:from].gsub(/#/, '') # If the id comes with #, we remove it
+      select2_container = find("#s2id_#{select_id}").click
       find(".select2-drop li", text: value).click
     end
 
     def select2_create_choice(value, options = {})
       raise "Must pass a hash containing 'from'" if not options.is_a?(Hash) or not options.has_key?(:from)
-      select_name = options[:from]
+      select_id = options[:from]
 
-      page.execute_script %Q{$('#{select_name}').select2('open')}
+      page.execute_script %Q{$('#{select_id}').select2('open')}
       page.execute_script "$('input.select2-input').val('#{value}').trigger('keyup-change');"
       sleep(1)
       page.execute_script(%Q{$("div.select2-result-label:contains('#{value}')").mouseup()})
